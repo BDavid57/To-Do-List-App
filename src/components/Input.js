44160e2761fea3ca52353f2b8fca74e3a2f6f320
BDavid57 <67975174+BDavid57@users.noreply.github.com>
2021-04-10 14:27:04 +0300

    Update Input.js

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,7 +1,5 @@
 
 const Input = (props) => {
-    const onChangeHandler = e => (props.updateCurrentItem(e.target.value))
-
     return <div>
         <h1>To Do List</h1>
             <div className="input-wrapper">
@@ -10,7 +8,7 @@ const Input = (props) => {
                     placeholder="Add Item"
                     value={props.currentItem}
                     onKeyDown={props.handleKeyDown}
-                    onChange={onChangeHandler}/>
+                    onChange={e => (props.updateCurrentItem(e.target.value))}/>
                 <button onClick={props.addItemToList}>Add</button>
             </div>
     </div>
