41573868d0a4f90305f0f01922aa0731e1355f18
BDavid57 <67975174+BDavid57@users.noreply.github.com>
2021-04-30 17:27:45 +0300

    Update Input.js

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,16 +1,33 @@
 
+
 const Input = (props) => {
+    const idForObj = () => (Math.floor(Math.random() * 100000))
+
+    const addItemToList = () => {
+        const newToDo = {
+            item: props.currentItem,
+            key: idForObj(),
+            date: null,
+            hour: null,
+        }
+
+        props.updateItemList([...props.itemList, newToDo])
+        props.updateCurrentItem('')
+    }
+
+    const handleKeyDown = e => (e.key === 'Enter' ? addItemToList() : null)
+
     return <div>
         <h1>To Do List</h1>
-            <div className="input-wrapper">
-                <input 
-                    type="text"
-                    placeholder="Add Item"
-                    value={props.currentItem}
-                    onKeyDown={props.handleKeyDown}
-                    onChange={e => (props.updateCurrentItem(e.target.value))}/>
-                <button onClick={props.addItemToList}>Add</button>
-            </div>
+        <div className="input-wrapper">
+            <input
+                type="text"
+                placeholder="Add Item"
+                value={props.currentItem}
+                onKeyDown={handleKeyDown}
+                onChange={e => (props.updateCurrentItem(e.target.value))} />
+            <button onClick={props.addItemToList}>Add</button>
+        </div>
     </div>
 }
 
