1c942e977c5fd0bb7bd75ca047cc9e325772054f
BDavid57 <67975174+BDavid57@users.noreply.github.com>
2021-04-30 21:15:14 +0300

    added date and time

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,18 +1,22 @@
-
+import { Button } from '@material-ui/core';
+import { TextField } from '@material-ui/core';
 
 const Input = (props) => {
     const idForObj = () => (Math.floor(Math.random() * 100000))
 
     const addItemToList = () => {
         const newToDo = {
-            item: props.currentItem,
             key: idForObj(),
-            date: null,
-            hour: null,
+            item: props.currentItem,
+            date: props.currentDate,
+            hour: props.currentTime,
         }
 
         props.updateItemList([...props.itemList, newToDo])
         props.updateCurrentItem('')
+        props.setCurrentDate('')
+        props.setCurrentTime('')
+        console.log(props.itemList)
     }
 
     const handleKeyDown = e => (e.key === 'Enter' ? addItemToList() : null)
@@ -20,13 +24,32 @@ const Input = (props) => {
     return <div>
         <h1>To Do List</h1>
         <div className="input-wrapper">
-            <input
-                type="text"
-                placeholder="Add Item"
+            <TextField
+                className="item-text"
+                variant="standard"
+                label="Add Item"
+                InputLabelProps={{ shrink: true }}
                 value={props.currentItem}
                 onKeyDown={handleKeyDown}
                 onChange={e => (props.updateCurrentItem(e.target.value))} />
-            <button onClick={props.addItemToList}>Add</button>
+        </div>
+        <div className="date-time-wrapper">
+            <div className="date-and-time">
+                <div className="date">
+                    <TextField
+                        value={props.currentDate}
+                        type="date"
+                        onChange={e => (props.setCurrentDate(e.target.value))} />
+                </div>
+
+                <div className="time">
+                    <TextField
+                        value={props.currentTime}
+                        type="time"
+                        onChange={e => (props.setCurrentTime(e.target.value))} />
+                </div>
+            </div>
+            <Button onClick={addItemToList}>Add</Button>
         </div>
     </div>
 }
